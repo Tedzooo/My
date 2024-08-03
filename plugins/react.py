535d7bac99a312ce31671146ef908f87521dbe9a
TEDZOBOT <94851911+Tedzo555@users.noreply.github.com>
2024-08-03 14:59:16 +0530

    Update react.py

--- plugins/react.py
+++ plugins/react.py
@@ -1,4 +1,5 @@
 from pyrogram import Client, filters
+from pyrogram.types import Message
 from pyrogram.errors import MessageIdInvalid, ChatAdminRequired, EmoticonInvalid, ReactionInvalid 
 from random import choice
 
